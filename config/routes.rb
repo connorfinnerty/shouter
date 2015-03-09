95c1b565c80910b271f9e2ac51d0ed20488480f2
Connor Finnerty <connorfinnerty@gmail.com>
2015-03-09 12:05:22 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+require "monban/constraints/signed_in"
+
+Rails.application.routes.draw do
+
+  constraints Monban::Constraints::SignedIn.new do
+    root "dashboards#show", as: :dashboard
+  end
+
+  root  "welcome#index"
+
+  resources :shouts, only: [:create, :show]
+  resources :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+end
